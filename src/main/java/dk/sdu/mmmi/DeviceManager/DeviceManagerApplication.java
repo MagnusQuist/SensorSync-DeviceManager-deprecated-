66fb746c68f39e49d41e455fbc3f11f38c526adb
Magnus Quist <maqui21@student.sdu.dk>
2024-02-15 15:23:16 +0100

    initial commit

---
+++ src/main/java/dk/sdu/mmmi/DeviceManager/DeviceManagerApplication.java
@@ -0,0 +1,13 @@
+package dk.sdu.mmmi.DeviceManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dk/sdu/mmmi/DeviceManager/DeviceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.sdu.mmmi.DeviceManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
